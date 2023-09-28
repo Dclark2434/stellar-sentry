05df2e276912172a27c2e0962094f0469a5d1854
Dustin Clark <dclark2434@gmail.com>
2023-09-27 22:09:10 -0500

    added settings

--- stellar_sentry.py
+++ stellar_sentry.py
@@ -1,23 +1,26 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class StellarSentry:
 
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Stellar Sentry")
 
-        self.bg_color = (48, 20, 61)
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
 
