447cd68fce1ed6e8d7f3e2fafc7d3288f3f5e3c4
Dustin Clark <dclark2434@gmail.com>
2023-09-27 21:42:02 -0500

    initial commit

---
+++ stellar_sentry.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+
+class StellarSentry:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Stellar Sentry")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ss = StellarSentry()
+    ss.run_game()
