e1c79c13f77e550cac75265c37548cc3f65079d9
Dustin Clark <dclark2434@gmail.com>
2023-09-28 21:52:09 -0500

    added ability to fire bullets

--- settings.py
+++ settings.py
@@ -2,9 +2,15 @@ class Settings:
     # A class to store all settings for Stellar Sentry.
     def __init__(self):
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 1080
         self.bg_color = (48, 20, 61)
 
-       # Player settings
+        # Player settings
         self.player_speed = 5
+
+        # Bullet settings
+        self.bullet_speed = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
--- stellar_sentry.py
+++ stellar_sentry.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from player import Player
+from bullet import Bullet
 
 class StellarSentry:
 
@@ -17,12 +18,14 @@ class StellarSentry:
         pygame.display.set_caption("Stellar Sentry")
 
         self.player = Player(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         # Start the main loop for the game.
         while True:
             self._check_events()
             self.player.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -43,6 +46,11 @@ class StellarSentry:
             self.player.moving_right = True
         elif event.key == pygame.K_a:
             self.player.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_d:
@@ -53,6 +61,8 @@ class StellarSentry:
     def _update_screen(self): 
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.player.blitme()
 
         pygame.display.flip()
