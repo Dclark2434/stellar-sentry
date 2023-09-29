8b8f8394340be9dc16a0443b86efb2c92ecca450
Dustin Clark <dclark2434@gmail.com>
2023-09-28 22:42:27 -0500

    refactored

--- settings.py
+++ settings.py
@@ -10,7 +10,8 @@ class Settings:
         self.player_speed = 5
 
         # Bullet settings
-        self.bullet_speed = 5
-        self.bullet_width = 3
+        self.bullet_speed = 8
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
--- stellar_sentry.py
+++ stellar_sentry.py
@@ -25,7 +25,7 @@ class StellarSentry:
         while True:
             self._check_events()
             self.player.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -49,8 +49,7 @@ class StellarSentry:
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_d:
@@ -66,6 +65,22 @@ class StellarSentry:
         self.player.blitme()
 
         pygame.display.flip()
+
+    def _update_bullets(self):
+        # Update position of bullets and get rid of old bullets.
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _fire_bullet(self):
+        # Create a new bullet and add it to the bullets group.
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
 if __name__ == '__main__':
     ss = StellarSentry()
